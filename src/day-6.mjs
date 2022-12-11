32d62eddcc757dc2b025271e5e7850b4f6695c56
Elizabeth Urquhart <me@ell.gay>
2022-12-11 18:52:24 +1300

    correct whitespace on day 6

--- src/day-6.mjs
+++ src/day-6.mjs
@@ -1,21 +1,21 @@
-    import { getInput } from './util.mjs';
+import { getInput } from './util.mjs';
 
-    const input = [...getInput(6)]
+const input = [...getInput(6)]
 
-    for(let i = 0; i < input.length; i++) {
-        let x = new Set(input.slice(i, i+4))
+for(let i = 0; i < input.length; i++) {
+    let x = new Set(input.slice(i, i+4))
 
-        if(x.size  === 4) {
-            console.log(i+4)
-            break
-        }
+    if(x.size  === 4) {
+        console.log(i+4)
+        break
     }
+}
 
-    for(let i = 0; i < input.length; i++) {
-        let x = new Set(input.slice(i, i+14))
+for(let i = 0; i < input.length; i++) {
+    let x = new Set(input.slice(i, i+14))
 
-        if(x.size  === 14) {
-            console.log(i+14)
-            break
-        }
+    if(x.size  === 14) {
+        console.log(i+14)
+        break
     }
+}
